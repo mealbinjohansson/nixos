7e2717b6b58e8ee3b842cd6b288e90562b250bfe
Albin Johansson <me@albinjohansson.com>
2026-02-27 20:24:05 +0100

    Remove user password hash to keep users mutable

--- nixosModules/albin.nix
+++ nixosModules/albin.nix
@@ -10,7 +10,6 @@
       isNormalUser = true;
       description = "Albin";
       extraGroups = [ "networkmanager" "wheel" ];
-      hashedPassword = "$6$L20PMhDJQGNl1AtG$MMn5sY/QD/1ldLsJ27dImP/qbW3YpGTY2pnyec5IU.XHJtWllXz6iiW5z9I4O1znEyswEU5SqdgdH/AwZ1TtV1";
     };
   };
 }
