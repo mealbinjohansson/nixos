77991027b9fb4f2f95a01f2376d1cba9297974f1
Albin Johansson <me@albinjohansson.com>
2026-03-29 11:52:46 +0200

    Add automatic clean up of old generations

--- nixosModules/base.nix
+++ nixosModules/base.nix
@@ -9,7 +9,14 @@
 
   config = lib.mkIf config.base.enable {
     nix.settings.experimental-features = ["nix-command" "flakes"];
+    nix.gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 30d";
+    };
     nixpkgs.config.allowUnfree = true;
     nixpkgs.config.input-fonts.acceptLicense = true;
+
+    boot.loader.systemd-boot.configurationLimit = 10;
   };
 }
